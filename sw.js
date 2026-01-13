004dd34fe5fad9eedf2fb704becb00a5b9910016
Sadman Abid <107977336+helloimabid@users.noreply.github.com>
2026-01-14 03:30:47 +0600

    added monetag scripts

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10459981
+    "zoneId": 10460130
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
